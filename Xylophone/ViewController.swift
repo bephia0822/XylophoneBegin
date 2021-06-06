9380dfcf18e4cc36759c8a2671c9926d104bdfd1
bephia0822 <bephia0822@gmail.com>
2021-06-06 23:31:07 +0900

    Initial Commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by appsgaga on 2017/10/24.
+//  Copyright © 2017年 appsgaga. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var audioMatrix: [AVAudioPlayer?] = [AVAudioPlayer?]()
+    
+    
+    @IBAction func playSound(_ sender: UIButton) {
+        audioMatrix[sender.tag]?.stop()
+        audioMatrix[sender.tag]?.currentTime = 0.0
+        audioMatrix[sender.tag]?.play()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for i in 0...7{
+            guard let path = Bundle.main.path(forResource: "\(i+1)", ofType: "mp3") else{
+                audioMatrix.append(nil)
+                return
+            }
+            let url = URL(fileURLWithPath: path)
+            do{
+                audioMatrix.append(try AVAudioPlayer(contentsOf: url))
+            }catch{
+                audioMatrix.append(nil)
+            }
+        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
